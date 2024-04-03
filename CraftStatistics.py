c36622c60cb27c82f0dfb82894be5aa2129e240f
Daniel M <danthedestroy3r@gmail.com>
2024-04-02 21:07:21 -0500

    ROC graphs (broken)

--- CraftStatistics.py
+++ CraftStatistics.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 from Craft import *
 from Atmosphere import Atmosphere
@@ -11,6 +12,30 @@ class CraftStatistics():
     def __init__(self, Craft: Craft) -> None:
         self.StatsCraft = Craft
         self.Active_Atmosphere = Craft.Atmosphere
+
+    def weight_from_str(self, WEIGHT: str)-> float:
+        """Gets the crafts weight from a string.
+
+        Args:
+            WEIGHT (str): Either "TAKEOFF" for the provided crafts takeoff weight, "EMPTY" for empty weight , "AVE" average the TOW and EW, or a float (ie: 10.1). When a float is provided it will be cast from string to the float.
+
+        Returns:
+            float: weight in Newtons
+        """
+        if WEIGHT.upper() == "TAKEOFF":
+            weight = self.StatsCraft.weight_takeoff
+        elif WEIGHT.upper() == "EMPTY":
+            weight = self.StatsCraft.weight_empty
+        elif WEIGHT.upper() == "AVE":
+            weight = (self.StatsCraft.weight_empty + self.StatsCraft.weight_takeoff)/2
+        else:
+            try:
+                weight = float(WEIGHT)
+            except:
+                weight = 0
+                print("Conversion to float failed")
+        return weight
+
 
     #PART F
     def get_ThrustAvailable_jet(self,alt):
@@ -40,24 +65,14 @@ class CraftStatistics():
         Returns:
             Pr: Power requred 
         """        
-        if WEIGHT.upper() == "TAKEOFF":
-            weight = self.StatsCraft.weight_takeoff
-        elif WEIGHT.upper() == "EMPTY":
-            weight = self.StatsCraft.weight_empty
-        elif WEIGHT.upper() == "AVE":
-            weight = (self.StatsCraft.weight_empty + self.StatsCraft.weight_takeoff)/2
-        else:
-            try:
-                weight = float(WEIGHT)
-            except:
-                print("Conversion to float failed")
+        weight = self.weight_from_str(WEIGHT)
 
         dens = self.Active_Atmosphere.dens_trop_alt(alt)
         k = calc_K_value(self.StatsCraft.mainwing.OswaldE,self.StatsCraft.mainwing.AR)
         Pr = calc_PowerReq(dens,velocity,self.StatsCraft.mainwing.Area,self.StatsCraft.Cd0,k,weight)
         return Pr
     
-    def graph_PowerAval_vs_PowerReq(self,Alt_lower, Alt_upper,numPoints, Velocity, WEIGHT: str,GRAPH_EXCESS: bool = False):
+    def graph_PowerAval_vs_PowerReq(self,Alt_lower, Alt_upper,numPoints, Velocity, WEIGHT: str,GRAPH_EXCESS: bool = False,SENDRAW: bool = False):
         """Returns a MPL figure of power requred and available vs alt
 
         Args:
@@ -69,18 +84,7 @@ class CraftStatistics():
         Returns:
             plt: Plot
         """     
-
-        if WEIGHT.upper() == "TAKEOFF":
-            weight = self.StatsCraft.weight_takeoff
-        elif WEIGHT.upper() == "EMPTY":
-            weight = self.StatsCraft.weight_empty
-        elif WEIGHT.upper() == "AVE":
-            weight = (self.StatsCraft.weight_empty + self.StatsCraft.weight_takeoff)/2
-        else:
-            try:
-                weight = float(WEIGHT)
-            except:
-                print("Conversion to float failed")
+        weight = self.weight_from_str(WEIGHT)
         
         alt_array = np.linspace(Alt_lower,Alt_upper,num=numPoints) #Array of numbers between lower and upper (inclusive)
         prA_array = np.zeros(alt_array.shape)
@@ -89,15 +93,18 @@ class CraftStatistics():
             prA_array[i[0]]= self.get_PowerAvailable_jet(i[1],Velocity) /1000
             prR_array[i[0]]= self.get_PowerRequired_alt_jet(i[1],Velocity,str(weight)) /1000
 
-        fig, ax = plt.subplots()
         #ax.plot(alt_array,dens_array,linewidth=1,label="Dens")
         #ax.plot(alt_array,temp_array,linewidth=1,label="temp")
         if GRAPH_EXCESS:
             prEx_arr = prA_array - prR_array
+            if SENDRAW:
+                return [alt_array,prEx_arr]
+            fig, ax = plt.subplots()
             ax.plot(alt_array,prEx_arr,linewidth=2,label="Power Excess (kW)")
             strTitle = "Altitude vs Excess power"
             ax.hlines(0,Alt_lower,Alt_upper,colors="red",linestyles="dotted",label="Zero Excess")
         else:
+            fig, ax = plt.subplots()
             strTitle = "Altitude vs Power Available & Required"
             ax.plot(alt_array,prA_array,linewidth=2,label="Power Available (kW)")
             ax.plot(alt_array,prR_array,linewidth=2,label="Power Required (kW)")
@@ -147,6 +154,89 @@ class CraftStatistics():
 
         return fig
     
+    def get_ROC_vel_alt(self,alt,vel,WEIGHT):
+        powA = self.get_PowerAvailable_jet(alt,vel)
+        powR = self.get_PowerRequired_alt_jet(alt,vel,WEIGHT)
+        weight = self.weight_from_str(WEIGHT)
+        powEx = powA - powR
+        return (powEx/weight)
+
+    def graph_ROC(self,alt_Lower,alt_Upper,numPoints,Velocity,WEIGHT,INFEETMIN: bool = False,SENDRAW: bool = False)-> plt:
+        """Graphs rate of climb vs Altitude for the given velocity and weight.
+
+        Args:
+            alt_Lower (_type_): Lower limit for altitude (meters above sea level)
+            alt_Upper (_type_): Upper limit for altitude (meters above sea level)
+            numPoints (_type_): Number of points to graph
+            Velocity (_type_): Velocity to be used in excess energy calculation (m/s)
+            WEIGHT (_type_): Either "TAKEOFF" for the provided crafts takeoff weight, "EMPTY" for empty weight , "AVE" average the TOW and EW, or a float (newtons) (ie: 10.1). When a float is provided it will be cast from string to the float.
+            INFEETMIN (bool, optional): Graph in ROC in feet/min rather than m/s Defaults to False.
+
+        Returns:
+            _type_: A plot of ROC vs Altitide
+        """        
+        weight = self.weight_from_str(WEIGHT)
+    
+        PowerCurve = MyCraftStats.graph_PowerAval_vs_PowerReq(alt_Lower,alt_Upper,numPoints,Velocity,str(weight),GRAPH_EXCESS=True,SENDRAW=True)
+
+        """About  PowerCurve = [PowerCurve[0], (PowerCurve[1] * 1000)/weight] 
+        this is deviding every number in powercurve[1] (our excess power) by the weight. 
+        """
+
+        if INFEETMIN:
+            PowerCurve = [PowerCurve[0], ((PowerCurve[1] * 1000)/weight) * 3.28084 * 60] # Convert from m/s to ft/min
+            if SENDRAW: #If we want, we can send back the data without graphing it.
+                return PowerCurve
+            
+            fig, ax = plt.subplots()
+            ax.plot(PowerCurve[0],PowerCurve[1],linewidth=2,label="Rate of Climb (ft / m)")
+            ax.set(xlabel='Altitude (meters)', ylabel='Rate of Climb',title='Altitude vs RoC')
+            ax.hlines(100,PowerCurve[0][0],PowerCurve[0][len(PowerCurve[0]) - 1],colors="red",linestyles="dotted",label="100 ft/min")
+        else:
+            PowerCurve = [PowerCurve[0], (PowerCurve[1] * 1000)/weight] #Convert to m/s
+            if SENDRAW:
+                return PowerCurve
+            
+            fig, ax = plt.subplots()
+            ax.plot(PowerCurve[0],PowerCurve[1],linewidth=2,label="Rate of Climb (m/s)")
+            ax.set(xlabel='Altitude (meters)', ylabel='Rate of Climb',title='Altitude vs RoC')
+            ax.hlines(0.508,PowerCurve[0][0],PowerCurve[0][len(PowerCurve[0]) - 1],colors="red",linestyles="dotted",label="0.508 m/s")
+
+        return fig
+    
+    def graph_ROC_3d(self,alt_Lower,alt_Upper,numPoints,Velocity_min,Velocity_max,num_vel_points,WEIGHT,INFEETMIN: bool = False,SENDRAW: bool = False)-> plt:
+        """Returns a 3d plot of RATE OF CLIMB over ALTUDUDE and VELOCITY
+
+        Args:
+            alt_Lower (_type_): lower alt limit
+            alt_Upper (_type_): upper alt limit
+            numPoints (_type_): number of altitude points
+            Velocity_min (_type_): lower velocity limit
+            Velocity_max (_type_): upper velocity limit
+            num_vel_points (_type_): number of velocity points
+            WEIGHT (_type_): WEIGHT (_type_): Either "TAKEOFF" for the provided crafts takeoff weight, "EMPTY" for empty weight , "AVE" average the TOW and EW, or a float (newtons) (ie: 10.1). When a float is provided it will be cast from string to the float.
+            INFEETMIN (bool, optional): _description_. Defaults to False.
+            SENDRAW (bool, optional): _description_. Defaults to False.
+
+        Returns:
+            plt: 3d plot
+        """
+
+
+        alt_array = np.linspace(alt_Lower,alt_Upper,num=numPoints) #y
+        vel_array = np.linspace(Velocity_min,Velocity_max,num=num_vel_points)#x
+
+        X, Y = np.meshgrid(vel_array, alt_array)
+        Z = self.get_ROC_vel_alt(Y,X,WEIGHT)
+        fig = plt.figure()
+        ax = plt.axes(projection ='3d')
+        ax.plot_wireframe(X, Y, Z, color ='green')
+        return fig
+
+        
+        
+
+       
 
 if __name__ == "__main__":
     OppaStoppa = Craft("OpptaStoppa")
@@ -181,10 +271,23 @@ if __name__ == "__main__":
 
 
     MyCraftStats = CraftStatistics(OppaStoppa)
+    """
     ThrustAvailableCurve = MyCraftStats.graph_ThrustAvailable(0,10000,1000)
     ThrustAvailableCurve.legend()
+    #ThrustAvailableCurve.show()
 
     PowerCurve = MyCraftStats.graph_PowerAval_vs_PowerReq(0,10000,1000,76,"AVE",GRAPH_EXCESS=True)
     PowerCurve.legend()
+    #PowerCurve.show()
+
+    rcCurve = MyCraftStats.graph_ROC(0,10000,1000,76,"AVE")
+    rcCurve.legend()
+
+    rcfCurve = MyCraftStats.graph_ROC(0,10000,1000,76,"AVE",INFEETMIN=True)
+    rcfCurve.legend()
+    #rcCurve.show()
+    """
+    curve = MyCraftStats.graph_ROC_3d(0,10000,1000,50,90,1000,"AVE")
+    curve.legend()
     plt.show()
     
